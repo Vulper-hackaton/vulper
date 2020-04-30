445ef0988963c1217efd332ca6803db2565030bf
PedroSGui <pedro.souzagui021@gmail.com>
2020-04-30 12:37:03 +0100

    deu merda

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,6 @@
 <template>
-  <div id="body" class="container-fluid">
-    <div id="header" class="row justify-content-between">
-      <div id= "logo" class="col-4 rounded">
-      </div>
-      <div id= "name" class="col-2">
-      </div>
-    </div>
-
-    <div class="col pt-5"></div>
-    <div class="col pt-5"></div>
-    <div class="col pt-5"></div>
-    <div class="col pt-5"></div>
-    <div class="col pt-5"></div>
-    <div class="col pt-5"></div>
-    <div class="col pt-5"></div>
-    <div class="col pt-5">
-      <div class="row justify-content-md-center">
-          <div id="bg" class="col col-lg-3">
-            <button id="button1" type="button" class="btn rounded btn-block p-3">Defina seu perfil</button>
-          </div>
-      </div>
-
-      <div class="row p-4">
-        <div class="col col-4"></div>
-        <div class="col col-4 "><div class="row justify-content-md-center"><button id="button2" type="button" class="btn rounded btn-sm">Profissional</button></div></div>
-        <div class="col col-4"><div class="row justify-content-end"><button id="button3" type="button" class="btn rounded btn-sm">Acesso Direto</button></div></div>
-      </div>
-
-      
-    </div>
+  <div id="app">
+    <router-link to="/">Login</router-link>
   </div>
 </template>
 
-<style lang="scss">
-#logo {
-  background: transparent linear-gradient(90deg, #10C0C6 0%, #47D38C 100%, #086063 100%);
-}
-#name {
-  background: transparent linear-gradient(90deg, #10C0C6 0%, #47D38C 100%, #086063 100%);
-}
-
-#body{
-  background-color: #F1F1F1;
-  min-height: 100vh;
-}
-#button1{
-  background: transparent linear-gradient(270deg, #4DDE90 0%, #10C0C6 100%);
-  font-family: 'Lato',sans-serif;
-}
-#bg{
-}
-#button2{
-  background: transparent linear-gradient(270deg, #4DDE90 0%, #10C0C6 100%);
-  font-family: 'Lato',sans-serif;
-}
-#button3{
-  background: transparent linear-gradient(270deg, #4DDE90 0%, #10C0C6 100%);
-  font-family: 'Lato',sans-serif;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,48 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login'
-import Register from "../views/Register"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login";
+import Register from "../views/Register";
+import NewBroker from "../views/NewBroker";
 
 Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/login',
-    name: 'login',
-    component: Login
+    path: "/login",
+    name: "login",
+    component: Login,
   },
   {
-    path: '/register',
-    name: 'register',
-    component: Register
+    path: "/register",
+    name: "register",
+    component: Register,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/new-broker",
+    name: "NewBroker",
+    component: NewBroker,
+  },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
-export default router
+export default router;
