5a1d8129e9d8bce76cb8380e9c136fb8c28dd7a2
PedroSGui <pedro.souzagui021@gmail.com>
2020-05-02 13:22:13 +0100

    montando sutability

--- src/views/Suitability.vue
+++ src/views/Suitability.vue
@@ -1,15 +1,308 @@
 <template>
-    <div>
-        ROTEOU CERTO
+  <div id="home-container">
+    <div id="p1" class="m-3 row" style="margin: 0!important;">
+      <div id="p12" class="col col-1"></div>
+
+      <div id="p13" class="col col-10 p-5">
+        <form>
+          <div class="input-group mt-3 row">
+            <div class="col col-2 m-5 input-group-prepend mr-2">
+              <span class="input-group-text" id="basic-addon1"
+                >Qual sua faixa etária?</span
+              >
+            </div>
+            <div class="col col-8 pl-5">
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios1"
+                  value="option1"
+                  checked
+                />
+                <label class="form-check-label" for="exampleRadios1">
+                  25 anos ou menos.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  De 26 a 35.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  De 36 a 45.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  De 46 a 65.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  Mais de 65 anos.
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mt-3 row">
+            <div class="col col-2 m-5 input-group-prepend mr-2">
+              <span class="input-group-text" id="basic-addon1"
+                >Qual o seu patrimônio aproximado?
+              </span>
+            </div>
+            <div class="col col-8 pl-5 ml-5">
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios1"
+                  value="option1"
+                  checked
+                />
+                <label class="form-check-label" for="exampleRadios1">
+                  Menos de R$50 mil.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  Entre R$50 mil e R$250 mil.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  Entre R$250 mil e R$500 mil.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  Entre R$500 mil e R$750mil.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  Mais de R$750mil.
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mt-3 row">
+            <div class="col col-2 m-5 input-group-prepend mr-2">
+              <span class="input-group-text" id="basic-addon1"
+                >Qual sua renda média mensal?</span
+              >
+            </div>
+            <div class="col col-8 pl-5 ml-5">
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios1"
+                  value="option1"
+                  checked
+                />
+                <label class="form-check-label" for="exampleRadios1">
+                  Até R$4 mil.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  De R$4 mil até R$10 mil.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  De R$10mil até R$20 mil.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  De R$20mil até R$30 mil.
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  Mais de R$30 mil.
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mt-3 row">
+            <div class="col col-2 m-5  input-group-prepend mr-2">
+              <span class="input-group-text" id="basic-addon1"
+                >Já investiu?</span
+              >
+            </div>
+            <div class="col col-8 pl-5 ml-5">
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios1"
+                  value="option1"
+                  checked
+                />
+                <label class="form-check-label" for="exampleRadios1">
+                  Invisto
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  Já investi
+                </label>
+              </div>
+              <div class="form-check p-2 m-2">
+                <input
+                  class="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios2"
+                  value="option2"
+                />
+                <label class="form-check-label" for="exampleRadios2">
+                  Nunca Investi
+                </label>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div id="p14" class="col col-1"></div>
     </div>
+    <div class="row justify-content-center m-5">
+      <div class="pb-5 pl-2">
+        <router-link to="/suitability">
+          <button id="but0" type="submit" class="btn rounded">
+            Seguinte
+          </button>
+        </router-link>
+      </div>
+      <div class="pb-5 pl-2">
+        <router-link to="/edit-user">
+          <button id="but0" type="submit" class="btn rounded">
+            Sair
+          </button>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Suitability"
-    }
+export default {
+  name: "EditUser",
+};
 </script>
 
-<style scoped>
-
+<style lang="scss">
+#home-container {
+  background-color: #f1f1f1;
+}
+#p1,
+#p12 {
+  background-color: #f1f1f1;
+}
+#p13 {
+  background-color: #d1d1d1;
+}
+#but0 {
+  background: transparent linear-gradient(270deg, #4dde90 0%, #10c0c6 100%);
+  font-family: "Lato", sans-serif;
+}
 </style>
